d3a4850e065982339a754379ca4dfff9649aae49
Bai <bugatti_it@163.com>
2020-04-02 10:42:33 +0800

    [Update] 更改目录结构

--- omnidbclient/omnidbclient/settings.py
+++ omnidbclient/omnidbclient/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'service.apps.ServiceConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- omnidbclient/service/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import api
-
-app_name = 'service'
-
-urlpatterns = [
-    path('register-database-connection/', api.RegisterDatabaseConnection.as_view(), name='register-database-connection'),
-]
